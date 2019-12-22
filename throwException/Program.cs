c29be91b5b6807957ed7973f47c5b6c93537224a
Qui <lquiitfuture@gmail.com>
2019-12-22 21:22:12 +0700

    Update throw in Program.cs file

--- throwException/Program.cs
+++ throwException/Program.cs
@@ -2,11 +2,25 @@
 
 namespace throwException
 {
+    //before C# 7
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = Divide(10, 0);
+            Console.WriteLine("Press any key to exist.");
+            Console.ReadKey();
+        }
+        public static double Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+            {
+                return x % y;
+            }
         }
     }
 }
