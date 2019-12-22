3a53ca10ad9693c9615aab6ed32ee46a140a424a
Qui <lquiitfuture@gmail.com>
2019-12-22 21:17:40 +0700

    Add project files.

---
+++ throwException/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace throwException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
